e30c4a1b7633fc3085aed499306d91ecd636165c
Siva Sankar Challa <sivasankarchalla@Sivas-MacBook-Air.local>
2023-10-17 12:26:15 -0400

    new changes

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 package helloworld;
 
-public class main {
+public class first {
   public static void main(String[] args) {
     System.out.println("Hello, How are you Dude!");
     System.out.println("This is Sankar Challa!");
