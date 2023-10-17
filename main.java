ecf4d599f393fe8541c876b6f74c40c401d70968
Siva Sankar Challa <sivasankarchalla@Sivas-MacBook-Air.local>
2023-10-17 11:37:14 -0400

    First Java Program

---
+++ main.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class main {
+  public static void main(String[] args) {
+    System.out.println("Hello, How are Dude!");
+    System.out.println("This is Sankar Challa!");
+  }
+}
