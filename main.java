99833412723c44a6c6a2725fe5f345f42501206b
Siva Sankar Challa <sivasankarchalla@Sivas-MacBook-Air.local>
2023-10-17 12:07:53 -0400

    updated in main.java

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ package helloworld;
 
 public class main {
   public static void main(String[] args) {
-    System.out.println("Hello, How are Dude!");
+    System.out.println("Hello, How are you Dude!");
     System.out.println("This is Sankar Challa!");
     System.out.println("Testing");
   }
